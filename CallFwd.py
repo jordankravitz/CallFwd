60be13758b62b3adf497b18a7b59ae513702118e
[jordankravitz] <[jkravitz@usesi.com]>
2021-05-26 09:11:48 -0400

    Troubleshooting Jenkins issues - adding if exist into 2nd login

--- CallFwd.py
+++ CallFwd.py
@@ -25,10 +25,10 @@ r.wait(5)
 
 #Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
 #r.keyboard('[tab]')
-r.type('searchString0', '8004000')
+r.type('searchString0', '8003560')
 r.click('findButton')
 r.wait(5)
-r.click('8004000') 
+r.click('8003560') 
 r.wait(5)
 
 r.type('cfadestination', '8003535')
@@ -49,18 +49,21 @@ if r.exist('Proceed to 172.16.1.15 (unsafe)'):
     r.click('Proceed to 172.16.1.15 (unsafe)')
 
 #Login into CUCM with stored LDAP Username/PW
-r.type('//*[@name="j_username"]', un) 
-r.type('//*[@name="j_password"]',pw)
-r.click('cuesLoginButton') 
+if r.exist('//*[@name="j_username"]'):
+    r.type('//*[@name="j_username"]', un) 
+if r.exist('//*[@name="j_password"]'):
+    r.type('//*[@name="j_password"]',pw)
+if r.exist('cuesLoginButton'):
+    r.click('cuesLoginButton') 
 r.wait(5)
 
 
 #Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
 #r.keyboard('[tab]')
-r.type('searchString0', '8006000')
+r.type('searchString0', '8003560')
 r.click('findButton')
 r.wait(5)
-r.click('8006000') 
+r.click('8003560') 
 r.wait(5)
 
 r.type('cfadestination', '8003535')
