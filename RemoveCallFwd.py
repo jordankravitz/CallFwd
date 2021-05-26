10702dd6dd3b97368770efe18d50d5df23442c36
[jordankravitz] <[jkravitz@usesi.com]>
2021-05-26 11:29:33 -0400

    Updated the remove callfwd script

---
+++ RemoveCallFwd.py
@@ -0,0 +1,71 @@
+import rpa as r
+
+#un = input ("What is your LDAP Username? ")
+#pw = input("What is your ldap password? ")
+
+un = 'Automation'
+pw = 'vgYf9UeX7n23'
+
+r.init()
+r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
+
+#Only for First time Login
+if r.exist('Advanced'):
+    r.click('Advanced')
+if r.exist('Proceed to 172.16.1.15 (unsafe)'):
+    r.click('Proceed to 172.16.1.15 (unsafe)')
+
+#Login into CUCM with stored LDAP Username/PW
+if r.exist('j_username'):
+    r.type('j_username', un) 
+if r.exist('j_password'):
+    r.type('j_password',pw)
+if r.exist('cuesLoginButton'):
+    r.click('cuesLoginButton') 
+r.wait(10)
+
+r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
+
+#Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
+#r.keyboard('[tab]')
+r.type('searchString0', '8003560')
+r.click('findButton')
+r.wait(5)
+r.click('8003560') 
+r.wait(5)
+
+r.type('cfadestination', '[clear]')
+
+r.click('Save')
+r.wait(5)
+
+
+#Operator Extension 8004000 - adding callfwd to send calls to 8003535 - inside sales
+
+r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
+'''
+#Only for First time Login
+r.click('Advanced')
+r.click('Proceed to 172.16.1.15 (unsafe)')
+
+#Login into CUCM with stored LDAP Username/PW
+r.type('//*[@name="j_username"]', un) 
+r.type('//*[@name="j_password"]',pw)
+r.click('cuesLoginButton') 
+r.wait(5)
+'''
+
+#Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
+r.type('searchString0', '8003640')
+r.click('findButton')
+r.wait(5)
+r.click('8003640') 
+r.wait(5)
+
+r.type('cfadestination', '[clear]')
+
+r.click('Save')
+r.wait(5)
+
+r.close()
+
