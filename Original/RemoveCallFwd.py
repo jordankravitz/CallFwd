6dffc56b9111040dd16aa6d4cff2a5e136a8ae28
[jordankravitz] <[jkravitz@usesi.com]>
2021-05-26 11:27:06 -0400

    Updated the remove callfwd script

--- Original/RemoveCallFwd.py
+++ Original/RemoveCallFwd.py
@@ -10,35 +10,40 @@ r.init()
 r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
 
 #Only for First time Login
-r.click('Advanced')
-r.click('Proceed to 172.16.1.15 (unsafe)')
+if r.exist('Advanced'):
+    r.click('Advanced')
+if r.exist('Proceed to 172.16.1.15 (unsafe)'):
+    r.click('Proceed to 172.16.1.15 (unsafe)')
 
 #Login into CUCM with stored LDAP Username/PW
-r.type('//*[@name="j_username"]', un) 
-r.type('//*[@name="j_password"]',pw)
-r.click('cuesLoginButton') 
-r.wait(5)
+if r.exist('j_username'):
+    r.type('j_username', un) 
+if r.exist('j_password'):
+    r.type('j_password',pw)
+if r.exist('cuesLoginButton'):
+    r.click('cuesLoginButton') 
+r.wait(10)
 
+r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
 
 #Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
 #r.keyboard('[tab]')
-r.type('searchString0', '8004000')
+r.type('searchString0', '8003560')
 r.click('findButton')
 r.wait(5)
-r.click('8004000') 
+r.click('8003560') 
 r.wait(5)
 
 r.type('cfadestination', '[clear]')
 
 r.click('Save')
-r.wait('5')
-r.close()
+r.wait(5)
 
 
 #Operator Extension 8004000 - adding callfwd to send calls to 8003535 - inside sales
-r.init()
-r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
 
+r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
+'''
 #Only for First time Login
 r.click('Advanced')
 r.click('Proceed to 172.16.1.15 (unsafe)')
@@ -48,20 +53,19 @@ r.type('//*[@name="j_username"]', un)
 r.type('//*[@name="j_password"]',pw)
 r.click('cuesLoginButton') 
 r.wait(5)
-
+'''
 
 #Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
-#r.keyboard('[tab]')
-r.type('searchString0', '8006000')
+r.type('searchString0', '8003640')
 r.click('findButton')
 r.wait(5)
-r.click('8006000') 
+r.click('8003640') 
 r.wait(5)
 
 r.type('cfadestination', '[clear]')
 
 r.click('Save')
-r.wait('5')
+r.wait(5)
 
 r.close()
 
