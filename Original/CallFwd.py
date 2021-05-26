0043328b6c8b5acbdc35625d2a295f9045451a5e
[jordankravitz] <[jkravitz@usesi.com]>
2021-05-26 09:55:17 -0400

    still testing jenkins stuff

--- Original/CallFwd.py
+++ Original/CallFwd.py
@@ -1,5 +1,4 @@
 import rpa as r
-from tagui import exist
 
 #un = input ("What is your LDAP Username? ")
 #pw = input("What is your ldap password? ")
@@ -11,68 +10,58 @@ r.init()
 r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
 
 #Only for First time Login
-if r.exist('Advanced'):
-    r.click('Advanced')
-if r.exist('Proceed to 172.16.1.15 (unsafe)'):
-    r.click('Proceed to 172.16.1.15 (unsafe)')
+r.click('Advanced')
+r.click('Proceed to 172.16.1.15 (unsafe)')
 
 #Login into CUCM with stored LDAP Username/PW
-if r.exist('j_username'):
-    r.type('j_username', un) 
-if r.exist('j_password'):
-    r.type('j_password',pw)
-if r.exist('cuesLoginButton'):
-    r.click('cuesLoginButton') 
+r.type('//*[@name="j_username"]', un) 
+r.type('//*[@name="j_password"]',pw)
+r.click('cuesLoginButton') 
 r.wait(5)
 
 
 #Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
 #r.keyboard('[tab]')
-r.type('searchString0', '8003560')
+r.type('searchString0', '8004000')
 r.click('findButton')
 r.wait(5)
-r.click('8003560') 
+r.click('8004000') 
 r.wait(5)
 
 r.type('cfadestination', '8003535')
 
 r.click('Save')
-r.wait(5)
+r.wait('5')
 r.close()
-r.wait(5)
+
 
 #Operator Extension 8004000 - adding callfwd to send calls to 8003535 - inside sales
 r.init()
 r.url('https://172.16.1.15/ccmadmin/directoryNumberFindList.do')
 
 #Only for First time Login
-if r.exist('Advanced'):
-    r.click('Advanced')
-if r.exist('Proceed to 172.16.1.15 (unsafe)'):
-    r.click('Proceed to 172.16.1.15 (unsafe)')
+r.click('Advanced')
+r.click('Proceed to 172.16.1.15 (unsafe)')
 
 #Login into CUCM with stored LDAP Username/PW
-if r.exist('j_username'):
-    r.type('j_username', un) 
-if r.exist('j_password'):
-    r.type('j_password',pw)
-if r.exist('cuesLoginButton'):
-    r.click('cuesLoginButton') 
+r.type('//*[@name="j_username"]', un) 
+r.type('//*[@name="j_password"]',pw)
+r.click('cuesLoginButton') 
 r.wait(5)
 
 
 #Operator Extension 8006000 - adding callfwd to send calls to 8003535 - inside sales
 #r.keyboard('[tab]')
-r.type('searchString0', '8003560')
+r.type('searchString0', '8006000')
 r.click('findButton')
 r.wait(5)
-r.click('8003560') 
+r.click('8006000') 
 r.wait(5)
 
 r.type('cfadestination', '8003535')
 
 r.click('Save')
-r.wait(5)
+r.wait('5')
 
 r.close()
 
